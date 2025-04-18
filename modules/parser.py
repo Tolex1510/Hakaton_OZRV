15ffe84aa648412dcdb8f12ef55d6f1675313bc5
Tracebacker <dsrase201@gmail.com>
2025-04-18 19:55:38 +0300

    add manual parser

--- modules/parser.py
+++ modules/parser.py
@@ -1,7 +1,13 @@
 import pandas as pd
 from event import Event
+from datetime import datetime
 
-def parse(path):
+def parseManually(path):
+    """
+    Parse csv file for learning AI and first case
+    :param path: Path of csv file
+    :return: List with Event objects of each row in csv file with attributes: 'time', 'incident' and 'node_memory_MemFree_bytes'
+    """
     data = pd.read_csv(path)[["time", "incident", "node_memory_MemFree_bytes"]]
 
     result = []
@@ -11,10 +17,35 @@ def parse(path):
     # row[2] - node_memory_MemFree_bytew
 
     for row in data.values:
+        time = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
         result.append(Event(
-            time = row[0],
+            time = time,
             incident = row[1],
             node_memory_MemFree_bytes = row[2]
         ))
     
+    return result
+
+def parse(path):
+    """
+    Parse csv file for second case and
+    :param path: Path of csv file
+    :return: List with Event objects of each row in csv file with attributes: 'time' and 'node_memory_MemFree_bytes'
+    """
+    data = pd.read_csv(path)[["time", "node_memory_MemFree_bytes"]]
+
+    result = []
+
+    # row[0] - time
+    # row[1] - node_memory_MemFree_bytes
+
+    for row in data.values:
+        # transfer string to datetime
+        time = datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S")
+        result.append(Event(
+            time=time,
+            incident=None,
+            node_memory_MemFree_bytes=row[1]
+        ))
+
     return result
