0903012688dae58991f2de9fc7b6892da74ed7ac
Tracebacker <dsrase201@gmail.com>
2025-04-18 20:01:03 +0300

    change hard code

--- modules/parser.py
+++ modules/parser.py
@@ -2,13 +2,15 @@ import pandas as pd
 from event import Event
 from datetime import datetime
 
+ATTRIBUTES = ["time", "incident", "node_memory_MemFree_bytes"]
+
 def parseManually(path):
     """
     Parse csv file for learning AI and first case
     :param path: Path of csv file
     :return: List with Event objects of each row in csv file with attributes: 'time', 'incident' and 'node_memory_MemFree_bytes'
     """
-    data = pd.read_csv(path)[["time", "incident", "node_memory_MemFree_bytes"]]
+    data = pd.read_csv(path)[ATTRIBUTES]
 
     result = []
 
@@ -32,7 +34,7 @@ def parse(path):
     :param path: Path of csv file
     :return: List with Event objects of each row in csv file with attributes: 'time' and 'node_memory_MemFree_bytes'
     """
-    data = pd.read_csv(path)[["time", "node_memory_MemFree_bytes"]]
+    data = pd.read_csv(path)[[elem for elem in ATTRIBUTES if elem != "incident"]]
 
     result = []
 
