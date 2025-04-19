7c8c77781762e73c9fdf7bc9f1fdc345b843f75d
Tracebacker <dsrase201@gmail.com>
2025-04-19 09:51:34 +0300

    good commit

--- Backend/Modules/parser.py
+++ Backend/Modules/parser.py
@@ -1,30 +1,6 @@
 import pandas as pd
 from Modules.event import Event
 from datetime import datetime
-from typing import List
-from flask import make_response
-import csv
-from io import StringIO
-
-ATTRIBUTES = ["time", "incident", "node_memory_MemFree_bytes", "node_cpu_seconds_total",
-              "node_disk_read_bytes_total", "node_network_receive_errs_total", "node_processes_pids"]
-
-def parseManually(path: str) -> List[Event]:
-    """
-    Parse csv file for learning AI and first case
-    :param path: Path of csv file
-    :return: List with Event objects of each row in csv file with attributes: 'time', 'incident' and 'node_memory_MemFree_bytes'
-    """
-    data = pd.read_csv(path)[["time", "incident", "node_memory_MemFree_bytes"]]
-
-    result = []
-
-    for row in data.values:
-        # transfer string to datetime
-        elem = [datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S"), row[1], row[2]]
-        result.append(elem)
-
-    return delta_parse(result)
 
 def parse(path, attrs=None):
     """
@@ -55,19 +31,3 @@ def delta_parse(parse_list):
         memory_dif = parse_list[i + 1][1] - parse_list[i][1]
         result.append([parse_list[i][0], memory_dif])
     return result
-
-def parse_csv_arr(arr):
-    data = StringIO()
-
-    writer = csv.writer(data)
-    writer.writerows(arr)
-
-    return data.getvalue()
-
-def return_csv_response(body):
-    si = StringIO()
-    cw = csv.writer(si)
-    cw.writerows(body)
-
-    # response = make_response(si.getvalue())
-    return si.getvalue()
